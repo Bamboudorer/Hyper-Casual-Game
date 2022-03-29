f1f56e33ea6568bb8d408c8c84e70492e8133c60
Bamboudorer <lucas.techer@epitech.eu>
2022-03-29 13:25:43 +0400

    Add some collision and reset positions

--- Assets/Scripts/PlayerMovements.cs
+++ Assets/Scripts/PlayerMovements.cs
@@ -6,10 +6,15 @@ using UnityEngine.Events;
 public class PlayerMovements : MonoBehaviour
 {
     // Determine the speed of the boat, can be change for the tests
-    [SerializeField] private float boatSpeed = 5f;
+    [SerializeField] private float boatSpeed = 2.5f;
     [SerializeField] private SwipeDetector swipeDetector;
     public bool rightToPlay = false;
     private Rigidbody rb;
+
+    [SerializeField] private float maxRightField = 16f;
+    [SerializeField] private float minLeftField = -16f;
+    [SerializeField] private float maxUpField = 26f;
+    [SerializeField] private float minDownField = -26f;
 
     void Start()
     {
@@ -20,12 +25,13 @@ public class PlayerMovements : MonoBehaviour
         }
     }
 
-
     void Update()
     {
         if (rightToPlay) {
             swipeDetector.CheckTouchPhase();
         }
+
+        CheckOutFieldPosition();
     }
 
     /// <summary>
@@ -35,7 +41,8 @@ public class PlayerMovements : MonoBehaviour
     /// <param name="tmpStart">First Input</param>
     /// <param name="tmpEnd">Last Input</param>
     /// <param name="newMagnitude">Magnitude to impulse power</param>
-    private void BoatMovement(Vector2 startPos, Vector2 endPos, float magnitude) {
+    private void BoatMovement(Vector2 startPos, Vector2 endPos, float magnitude)
+    {
         float magnitudePower = magnitude;
         float newPosX = endPos.x - startPos.x;
         float newPosZ = endPos.y - startPos.y;
@@ -46,5 +53,41 @@ public class PlayerMovements : MonoBehaviour
 
         Vector3 force = finalPos * magnitudePower * boatSpeed;
         rb.AddForce(force, ForceMode.Impulse);
+    }
+
+    private void CheckOutFieldPosition()
+    {
+        if (transform.position.x < minLeftField || transform.position.x > maxRightField) {
+            ResetPosition();
+        }
+        if (transform.position.z < minDownField || transform.position.z > maxUpField) {
+            ResetPosition();
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Wall") {
+            ResetVelocity();
+        }
+    }
+
+    /// <summary>
+    /// Reset the velocity, reset the speed of the boat to 0
+    /// </summary>
+    private void ResetVelocity()
+    {
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+    }
+
+    /// <summary>
+    /// Reset to position to the center of the game
+    /// X = 0 | Y = 0.5 | Z = 0
+    /// </summary>
+    private void ResetPosition()
+    {
+        ResetVelocity();
+        transform.position = new Vector3(0f, 0.5f, 0f);
     }
 }
